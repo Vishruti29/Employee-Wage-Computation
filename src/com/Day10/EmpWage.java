eff39bd94b2495eb969fdd54c7ffef4c49923d44
Vishruti Patil <vishruti2908@gmail.com>
2023-03-03 01:01:11 +0530

    part time emp condition added

--- src/com/Day10/EmpWage.java
+++ src/com/Day10/EmpWage.java
@@ -2,20 +2,26 @@ package com.Day10;
 import java.util.Random;
 public class EmpWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
         //Branch UC1_Emp_Check
         //Branch UC2_Emp_Daily_Wage
-
+        //Branch UC3_Add_Part_time_Emp_wage
         Random random = new Random ();
-        int emp_check = random.nextInt(2);
+        int emp_check = random.nextInt(3);
         int WagePerHr = 20;
         int FulldayHrs = 8;
+        int PartTimeHrs = 4;
         if(emp_check==0) {
             System.out.println("Employee is Absent");
         }
+        else if(emp_check==1){
+            System.out.println("Employee is working Part Time");
+            int dailyWage = WagePerHr*PartTimeHrs;
+            System.out.println("Employee Daily Wage is " + dailyWage);
+
+        }
         else {
             System.out.println("Employee is Present");
-            int dailyWage = WagePerHr * FulldayHrs;
+            int dailyWage = WagePerHr*FulldayHrs;
             System.out.println("Employee Daily Wage is " + dailyWage);
         }
     }
