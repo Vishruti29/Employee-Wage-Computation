d9eccc030712b93ef4877ed1fa919b6ed98ecd37
Vishruti Patil <vishruti2908@gmail.com>
2023-03-03 01:03:49 +0530

    switch case used for calculation

--- src/com/Day10/EmpWage.java
+++ src/com/Day10/EmpWage.java
@@ -2,27 +2,34 @@ package com.Day10;
 import java.util.Random;
 public class EmpWage {
     public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
         //Branch UC1_Emp_Check
         //Branch UC2_Emp_Daily_Wage
         //Branch UC3_Add_Part_time_Emp_wage
         Random random = new Random ();
         int emp_check = random.nextInt(3);
-        int WagePerHr = 20;
-        int FulldayHrs = 8;
-        int PartTimeHrs = 4;
-        if(emp_check==0) {
-            System.out.println("Employee is Absent");
-        }
-        else if(emp_check==1){
-            System.out.println("Employee is working Part Time");
-            int dailyWage = WagePerHr*PartTimeHrs;
-            System.out.println("Employee Daily Wage is " + dailyWage);
+        int wagePerHr = 20;
+        int fulldayHrs = 8;
+        int partTimeHrs = 4;
+        //Branch UC4_Switch_Case
+        switch (emp_check)
+        {
+            case 0 :
+                System.out.println("Employee is Absent"); break;
 
-        }
-        else {
-            System.out.println("Employee is Present");
-            int dailyWage = WagePerHr*FulldayHrs;
-            System.out.println("Employee Daily Wage is " + dailyWage);
+            //Branch UC3_Part_time_Emp_wage
+            case 1 :
+                System.out.println("Employee is working Part Time");
+                int pt_Daily_wage = wagePerHr*partTimeHrs;
+                System.out.println("Employee Daily Wage is " + pt_Daily_wage); break;
+
+
+            case 2 :
+                System.out.println("Employee is present");
+                int ft_Daily_wage = wagePerHr*fulldayHrs;
+                System.out.println("Employee Daily Wage is " + ft_Daily_wage); break;
+
+
         }
     }
 
