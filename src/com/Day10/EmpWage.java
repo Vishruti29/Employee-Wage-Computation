c87d0b52a95711e27acbdebd52504421e7d4b847
Vishruti Patil <vishruti2908@gmail.com>
2023-03-03 01:05:35 +0530

    monthly wage calculation

--- src/com/Day10/EmpWage.java
+++ src/com/Day10/EmpWage.java
@@ -6,11 +6,13 @@ public class EmpWage {
         //Branch UC1_Emp_Check
         //Branch UC2_Emp_Daily_Wage
         //Branch UC3_Add_Part_time_Emp_wage
+        //Branch UC5_Wage_per_Month
         Random random = new Random ();
         int emp_check = random.nextInt(3);
-        int wagePerHr = 20;
-        int fulldayHrs = 8;
-        int partTimeHrs = 4;
+        int WagePerHr = 20;
+        int FulldayHrs = 8;
+        int PartTimeHrs = 4;
+        int TotalWorkingDays = 20;
         //Branch UC4_Switch_Case
         switch (emp_check)
         {
@@ -20,17 +22,20 @@ public class EmpWage {
             //Branch UC3_Part_time_Emp_wage
             case 1 :
                 System.out.println("Employee is working Part Time");
-                int pt_Daily_wage = wagePerHr*partTimeHrs;
-                System.out.println("Employee Daily Wage is " + pt_Daily_wage); break;
+                int PT_Daily_wage = WagePerHr*PartTimeHrs;
+                int PT_MonthlyWage = PT_Daily_wage*TotalWorkingDays;
+                System.out.println("Employee Daily Wage is " + PT_Daily_wage);
+                System.out.println("Employee Monthly Wage is " + PT_MonthlyWage); break;
 
 
             case 2 :
                 System.out.println("Employee is present");
-                int ft_Daily_wage = wagePerHr*fulldayHrs;
-                System.out.println("Employee Daily Wage is " + ft_Daily_wage); break;
+                int FT_Daily_wage = WagePerHr*FulldayHrs;
+                int FT_MonthlyWage = FT_Daily_wage*TotalWorkingDays;
+                System.out.println("Employee Daily Wage is " + FT_Daily_wage);
+                System.out.println("Employee Monthly Wage is " + FT_MonthlyWage); break;
 
 
         }
     }
-
 }
