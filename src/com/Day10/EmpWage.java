d1f3568c6b70344f82c69f18057faaa577d94ca0
Vishruti Patil <vishruti2908@gmail.com>
2023-03-03 00:54:44 +0530

    checking emp is present or absent

--- src/com/Day10/EmpWage.java
+++ src/com/Day10/EmpWage.java
@@ -1,7 +1,16 @@
 package com.Day10;
-
+import java.util.Random;
 public class EmpWage {
     public static void main(String[] args) {
-        System.out.println(" Welcome to Employee Wage Computation Program ");
+        System.out.println("Welcome to Employee Wage Computation Program");
+        //Branch UC1_Emp_Check
+        Random random = new Random ();
+        int emp_check = random.nextInt(2);
+        if(emp_check==0) {
+            System.out.println("Employee is Absent");
+        }
+        else {
+            System.out.println("Employee is Present");
+        }
     }
 }
